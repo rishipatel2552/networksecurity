11562293bc04fdef3df117afcc08b224adb9e318
Rishi Patel <rishipatel@macbookpro.mynetworksettings.com>
2025-04-13 23:29:42 -0400

    Model Trainer Created

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -29,6 +30,12 @@ if __name__=='__main__':
         datatransformationartifact=data_transformation.initiate_data_transformation()
         logging.info("Data Transformation Completed")
         print(datatransformationartifact)
+
+        logging.info("Model Training Started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=datatransformationartifact)
+        modeltrainerartifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
